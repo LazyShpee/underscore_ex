e50717490bccc12d293561f6e62f1d8bcaf7cecb
LazyShpee <comemureravaud@gmail.com>
2019-08-22 14:31:20 +0200

    Added verbose flag to su

--- lib/command/su.ex
+++ lib/command/su.ex
@@ -12,22 +12,64 @@ defmodule UnderscoreEx.Command.Su do
       options
       |> OptionParser.split()
       |> OptionParser.parse!(
-        switches: [user: :string, guild: :string, channel: :string],
-        aliases: [u: :user, g: :guild, c: :channel]
+        switches: [user: :string, guild: :string, channel: :string, verbose: :boolean],
+        aliases: [u: :user, g: :guild, c: :channel, v: :verbose]
       )
 
     {options, command}
   end
 
-  @impl true
-  def call(%{message: message, prefix: prefix}, {options, command}) do
-    content =
-      case command |> String.trim() do
-        <<"raw:", rest::binary>> -> rest
-        command -> "#{prefix}#{command}"
+  defp format_context(uid, cid, gid, _message) do
+    user =
+      with {:ok, user} <- Nostrum.Cache.UserCache.get(uid) do
+        "as #{user.username}"
+      else
+        _ -> "as user #{uid}"
       end
 
-    gid = "#{options[:guild] || message.guild_id}" |> String.to_integer()
+    channel =
+      with {:ok, channel} <- Nostrum.Cache.ChannelCache.get(cid) do
+        case channel.name do
+          nil ->
+            with %Nostrum.Struct.User{username: username} <- channel.recipients |> Enum.at(0) do
+              "in #{username}'s DMs"
+            else
+              _ -> "in user DMs #{uid}"
+            end
+
+          name ->
+            "in ##{name}"
+        end
+      else
+        _ -> "in channel #{cid}"
+      end
+
+    guild =
+      with false <- is_nil(gid),
+           {:ok, guild} <- Nostrum.Cache.GuildCache.get(gid) do
+        "in #{guild.name}"
+      else
+        true -> ""
+        _ -> "in guild `#{gid}`"
+      end
+
+    "#{user} #{channel} #{guild}"
+  end
+
+  @impl true
+  def call(%{message: message, prefix: prefix}, {options, command}) do
+    {type, content} =
+      case command |> String.trim() do
+        <<"raw:", rest::binary>> -> {:raw, rest}
+        command -> {:command, "#{prefix}#{command}"}
+      end
+
+    gid =
+      case options[:guild] do
+        nil -> message.guild_id
+        "nil" -> nil
+        id -> id |> String.to_integer()
+      end
 
     {:ok, uid} =
       if options[:user] do
@@ -52,6 +94,15 @@ defmodule UnderscoreEx.Command.Su do
       |> update_in([:content], fn _ -> "#{content}" end)
       |> update_in([:guild_id], fn _ -> gid end)
       |> update_in([:channel_id], fn _ -> cid end)
+
+    if options[:verbose] == true do
+      """
+      #{if type == :raw, do: "Saying", else: "Executing command"} #{
+        format_context(uid, cid, gid, message)
+      }
+      """
+      |> UnderscoreEx.Util.pipe_message(message)
+    end
 
     UnderscoreEx.Consumer.handle_event(
       {:MESSAGE_CREATE, struct!(%Nostrum.Struct.Message{}, tmp), nil}
