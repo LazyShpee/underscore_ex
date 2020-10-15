3f7e59c6b59e34bcfd931d24cbe90933b9ccb778
LazyShpee <comemureravaud@gmail.com>
2020-10-15 17:44:18 +0200

    Added functional stack

--- lib/command/stack.ex
+++ lib/command/stack.ex
@@ -2,37 +2,107 @@ defmodule UnderscoreEx.Command.Stack do
   use UnderscoreEx.Command
   alias UnderscoreEx.Schema.StackItem
   alias UnderscoreEx.Repo
+  import Ecto.Query
 
   @impl true
-  def predicates, do: [&UnderscoreEx.Predicates.bot_owner/1]
+  def parse_args(arg),
+    do: arg |> String.split(" ", parts: 2) |> Enum.map(&String.trim/1) |> List.to_tuple()
 
   @impl true
-  def parse_args(arg), do: arg |> String.split(" ", parts: 2) |> Enum.map(&String.trim/1) |> List.to_tuple()
+  def description, do: "Stack de trucs a faire qui seront peut etre jamais faits."
+
+  @push_ops ["+", "push"]
+  @pop_ops ["-", "pop"]
+  @show_ops ["?", "show", "chaud"]
+  @clear_ops ["clear"]
 
   @impl true
-  def description, do: "Here be dragons."
+  def usage, do: [
+    "<#{@push_ops |> Enum.join("|")}> <content>",
+    "<#{@pop_ops |> Enum.join("|")}> [index]",
+    "<#{@show_ops |> Enum.join("|")}>",
+    "<#{@clear_ops |> Enum.join("|")}>"
+  ]
+  defp format_item({%{content: content, inserted_at: inserted_at}, index}) do
+    _time =
+      inserted_at
+      |> Timex.to_datetime("Europe/Paris")
+      |> Timex.format!("{YYYY}-{0M}-{0D} at {h24}:{m}")
 
-  @impl true # Clear
-  def call(_ctx, {op}) when op in ["clear"] do
-    "Clear stack"
+    "`[#{String.pad_leading("#{index}", 3, "0")}] #{content}`"
   end
 
-  @impl true # Add
-  def call(_ctx, {op, arg}) when op in ["+", "push"] do
-    "Add #{arg} to stack"
+  # Clear
+  @impl true
+  def call(ctx, {op}) when op in ["clear"] do
+    from(i in StackItem,
+      where: i.user_id == ^ctx.message.author.id
+    )
+    |> Repo.delete_all()
+
+    "C'est tout bon chef, gg"
   end
 
-  @impl true # Remove
-  def call(_ctx, {op, arg}) when op in ["-", "pop"] do
-    "Remove #{arg} to stack"
+  # Add
+  @impl true
+  def call(ctx, {op, arg}) when op in ["+", "push"] do
+    {arg, _} = arg |> String.split_at(255)
+
+    StackItem.changeset(%StackItem{}, %{
+      user_id: ctx.message.author.id,
+      content:
+        arg |> String.replace(["`"], "'") |> String.split(["\n"], trim: true) |> Enum.join(";")
+    })
+    |> Repo.insert!()
+
+    "Ok bro, c'est push 🚀"
   end
 
-  @impl true # Show
-  def call(_ctx, {op}) when op in ["?", "show"] do
-    "Show stack"
+  # Remove
+  @impl true
+  def call(ctx, {op}) when op in ["-", "pop"] do
+    call(ctx, {op, "0"})
+  end
+
+  # Remove
+  @impl true
+  def call(ctx, {op, arg}) when op in ["-", "pop"] do
+    with {n, _} <- Integer.parse(arg),
+         [item] <-
+           from(i in StackItem,
+             where: i.user_id == ^ctx.message.author.id,
+             order_by: [desc: i.inserted_at],
+             limit: 1,
+             offset: ^n
+           )
+           |> Repo.all() do
+      item |> Repo.delete!()
+
+      "Yes, t'as claque #{format_item({item, n})} 👏"
+    else
+      :error -> "Mec, donne un vrai int stp ._."
+      [] -> "T'as pas autant d'items ptdr"
+    end
+  end
+
+  # Show
+  @impl true
+  def call(ctx, {op}) when op in ["?", "show", "chaud"] do
+    with items when items != [] <-
+           from(i in StackItem,
+             where: i.user_id == ^ctx.message.author.id,
+             order_by: [desc: i.inserted_at]
+           )
+           |> Repo.all() do
+      items
+      |> Enum.with_index()
+      |> Enum.map(&format_item/1)
+      |> Enum.join("\n")
+    else
+      _ -> "T'as rien sur ta stack vrer..."
+    end
   end
 
   @impl true
   def call(_, _), do: "Eh?"
-
 end
